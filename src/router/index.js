89f6dbe379fdbc3fd749e354a298a9c33e2353ca
Polina Deniskina <polinaoioi67@gmail.com>
2024-02-28 10:55:06 +0300

    feat: add components

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import PageBlog from '../components/PageBlog.vue'
+import PageDetails from '../components/PageDetails.vue'
 import PageHome from '../components/PageHome.vue'
-
+import PageNotFound from '../components/PageNotFound.vue'
+import PageProject from '../components/PageProject.vue'
+import PageProjectDetails from '../components/PageProjectDetails.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +14,31 @@ const routes = [
     path: '/',
     name: 'home',
     component: PageHome
+  },
+  {
+    path: '/blog',
+    name: 'blog',
+    component: PageBlog
+  },
+  {
+    path: '/details',
+    name: 'details',
+    component: PageDetails
+  },
+  {
+    path: '/project',
+    name: 'project',
+    component: PageProject
+  },
+  {
+    path: '/projectdetails',
+    name: 'projectdetails',
+    component: PageProjectDetails
+  },
+  {
+    path: '*',
+    name: '404',
+    component: PageNotFound
   }
 ]
 
